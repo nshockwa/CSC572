9b4a2837e7b8aa75fc134ee140ced34ad6e223e4
Nick Sciacqua <nick.sciacqua@gmail.com>
2018-10-01 19:28:03 -0700

    Gaussian Blur Baby
    TODO: make screen resolution a uniform

--- postprocessing/resources/frag_nolight.glsl
+++ postprocessing/resources/frag_nolight.glsl
@@ -9,7 +9,19 @@ layout(location = 0) uniform sampler2D tex;
 
 void main()
 {
+	float weights[] = { 0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216 };
+	float xp = 1. / 480.0;
 	vec3 texturecolor = texture(tex, fragTex).rgb;
+	texturecolor.r = pow(texturecolor.r, 5);
+	texturecolor.g = pow(texturecolor.g, 5);
+	texturecolor.b = pow(texturecolor.b, 5);
+	for (int i = -10; i < 10; i++)
+	{
+		vec3 col = texture(tex, fragTex + vec2(i*xp, 0), 0).rgb;
+		if (i == 0) continue;
+		int wi = abs(i);
+		texturecolor += col * weights[wi - 1];
+	}
 	color.rgb = texturecolor;
 	color.a=1;
 }
