e729b19546b5e6224162465f7e7b54aea46facbd
Nick Sciacqua <nick.sciacqua@gmail.com>
2018-10-08 20:56:30 -0700

    Copied DoF dir and implemented differed shading
    TODO: Fix lighting

---
+++ Program1_Differed_Shading/resources/simple_frag.glsl
@@ -0,0 +1,28 @@
+#version 450 core 
+
+layout(location = 0) out vec4 color;
+layout(location = 1) out vec4 worldpos;
+layout(location = 2) out vec4 normals;
+
+in vec3 fragPos;
+in vec2 fragTex;
+in vec3 fragNor;
+in vec4 fragViewPos;
+
+uniform vec3 campos;
+
+layout(location = 0) uniform sampler2D tex;
+layout(location = 1) uniform sampler2D tex2;
+
+
+void main()
+{
+	vec3 normal = normalize(fragNor);
+	vec3 texturecolor = texture(tex, fragTex).rgb;
+	
+	color = vec4(texturecolor, 1.0);
+	worldpos = vec4(fragPos, 1.0);
+	normals = vec4(normal, 1.0);
+	//viewpos.z*=-1;
+
+}
